1dc9218a6e554024eb604cbe02938b2c002de310
Antoine Luciani <antoine1fr@gmail.com>
2016-07-15 14:18:45 +0200

    Fix CLI

--- src/osborn.ml
+++ src/osborn.ml
@@ -1,35 +1,27 @@
-let launch working_dir = ()
+let build working_dir = ()
 
 module Cli = struct
   open Cmdliner
 
   let working_dir =
-    let doc = "Directory in which to create the blog." in
+    let doc = "Directory in which reside the sources of the  blog." in
     Arg.(
       value
       & opt dir "./"
       & info ["d"; "working-dir"] ~doc)
 
-  let init = Term.(const launch $ working_dir)
-
-  let init_info =
-    let doc = "Simple static blog generator written in OCaml." in
-    let man = [
-      `S "BUGS";
-      `P "Email bug reports to <antoine1fr at gmail dot com>."] in
-    Term.info "launch" ~doc ~man
+  let build = Term.(const build $ working_dir)
 
   let info =
     let doc = "Simple static blog generator written in OCaml." in
     let man = [
       `S "BUGS";
       `P "Email bug reports to <antoine1fr at gmail dot com>."] in
-    Term.info "launch" ~doc ~man
+    Term.info "Osborn" ~doc ~man
 end
 
 let () =
-  let launch = (Cli.init, Cli.info) in
-  let commands = [launch] in
-  match Cmdliner.Term.eval_choice launch commands with
+  let launch = (Cli.build, Cli.info) in
+  match Cmdliner.Term.eval launch with
   | `Error msg -> exit 1
   | _ -> exit 0
