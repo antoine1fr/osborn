e39e218e705d6f4990b4862aec60f7c40f73df67
Antoine Luciani <antoine1fr@gmail.com>
2016-07-16 21:35:59 +0200

    Process mustache and markdown layers of posts
    Related to gh-2.

--- src/build.ml
+++ src/build.ml
@@ -31,6 +31,24 @@ let get_posts working_dir =
       {client_path; source_path; build_path})
     |> Result.return
 
+let mustache_filter scope str =
+  let mustache = Mustache.of_string str in
+  Mustache.render mustache scope
+
+let markdown_filter str =
+  let markdown = Omd.of_string str in
+  Omd.to_html ~pindent:true markdown
+
+let process_post working_dir build_dir scope post =
+  Utils.read_file post.source_path >>= fun content ->
+  let html = content
+    |> mustache_filter scope
+    |> markdown_filter in
+  Utils.write_file post.build_path html
+
+let process_posts working_dir build_dir scope posts =
+  Result.traverse (process_post working_dir build_dir scope) posts
+
 let build working_dir =
   let build_dir = working_dir ^ "/_build" in
   get_posts working_dir >>= fun posts ->
@@ -38,4 +56,6 @@ let build working_dir =
     |> Conf.from_file
     |> Result.get_ok ~default:Conf.default in
   let scope = {conf; posts} |> scope_to_yojson |> Utils.ezjsonm_of_yojson in
-  process_index working_dir build_dir scope
+  process_index working_dir build_dir scope >>= fun () ->
+  process_posts working_dir build_dir scope posts >>= fun _ ->
+  Result.return ()
