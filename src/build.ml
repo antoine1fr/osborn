acb49766dba495347de47ca2fe7c414c6ba39b32
Antoine Luciani <antoine1fr@gmail.com>
2016-07-16 13:11:05 +0200

    Refactor Build.build function
    The build function is the entry point the whole blog's build, and will
    handle quite a few things in the near future.
    
    This patch moves the processing of index.html in its own function.

--- src/build.ml
+++ src/build.ml
@@ -4,13 +4,16 @@ type scope = {
   conf : Conf.t;
 } [@@deriving yojson]
 
+let process_index working_dir build_dir scope =
+  Utils.read_file (working_dir ^ "/index.html") >>= fun index_tpl_str ->
+  let index_tpl = Mustache.of_string index_tpl_str in
+  let index_html = Mustache.render index_tpl scope in
+  Utils.write_file (build_dir ^ "/index.html") index_html
+
 let build working_dir =
   let build_dir = working_dir ^ "/_build" in
   let conf = working_dir ^ "/config.sexp"
     |> Conf.from_file
     |> Result.get_ok ~default:Conf.default in
   let scope = {conf} |> scope_to_yojson |> Utils.ezjsonm_of_yojson in
-  Utils.read_file (working_dir ^ "/index.html") >>= fun index_tpl_str ->
-  let index_tpl = Mustache.of_string index_tpl_str in
-  let index_html = Mustache.render index_tpl scope in
-  Utils.write_file (build_dir ^ "/index.html") index_html
+  process_index working_dir build_dir scope
