54fdb3ee55da5ae7354c63c69d4a069ed072778f
Antoine Luciani <antoine1fr@gmail.com>
2016-07-16 12:53:10 +0200

    Convert index.html
    Related to gh-1.

--- src/osborn.ml
+++ src/osborn.ml
@@ -57,10 +57,15 @@ let write_file path content =
     Error msg
 
 let build working_dir =
-  let config = working_dir ^ "/config.sexp"
+  let build_dir = working_dir ^ "/_build" in
+  let conf = working_dir ^ "/config.sexp"
     |> Conf.from_file
     |> Result.get_ok ~default:Conf.default in
-  ()
+  let scope = {conf} |> scope_to_yojson |> ezjsonm_of_yojson in
+  read_file (working_dir ^ "/index.html") >>= fun index_tpl_str ->
+  let index_tpl = Mustache.of_string index_tpl_str in
+  let index_html = Mustache.render index_tpl scope in
+  write_file (build_dir ^ "/index.html") index_html
 
 module Cli = struct
   open Cmdliner
