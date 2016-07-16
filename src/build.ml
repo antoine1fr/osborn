cfa6dae91425bb8a589ac6ca36cf74c5546fb63a
Antoine Luciani <antoine1fr@gmail.com>
2016-07-16 15:30:37 +0200

    Add list of posts to Mustache's JSON scope
    Related to gh-2.

--- src/build.ml
+++ src/build.ml
@@ -1,7 +1,12 @@
 open Std
 
+type post = {
+  path : string;
+} [@@deriving yojson]
+
 type scope = {
   conf : Conf.t;
+  posts : post list;
 } [@@deriving yojson]
 
 let process_index working_dir build_dir scope =
@@ -26,5 +31,5 @@ let build working_dir =
   let conf = working_dir ^ "/config.sexp"
     |> Conf.from_file
     |> Result.get_ok ~default:Conf.default in
-  let scope = {conf} |> scope_to_yojson |> Utils.ezjsonm_of_yojson in
+  let scope = {conf; posts} |> scope_to_yojson |> Utils.ezjsonm_of_yojson in
   process_index working_dir build_dir scope
