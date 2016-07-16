41446b6bdbdeb11d109692adb0f1bda3b350e3f8
Antoine Luciani <antoine1fr@gmail.com>
2016-07-16 13:06:28 +0200

    Move build function in new Build module

--- src/osborn.ml
+++ src/osborn.ml
@@ -1,20 +1,5 @@
 open Std
 
-type scope = {
-  conf : Conf.t;
-} [@@deriving yojson]
-
-let build working_dir =
-  let build_dir = working_dir ^ "/_build" in
-  let conf = working_dir ^ "/config.sexp"
-    |> Conf.from_file
-    |> Result.get_ok ~default:Conf.default in
-  let scope = {conf} |> scope_to_yojson |> Utils.ezjsonm_of_yojson in
-  Utils.read_file (working_dir ^ "/index.html") >>= fun index_tpl_str ->
-  let index_tpl = Mustache.of_string index_tpl_str in
-  let index_html = Mustache.render index_tpl scope in
-  Utils.write_file (build_dir ^ "/index.html") index_html
-
 module Cli = struct
   open Cmdliner
 
@@ -25,7 +10,7 @@ module Cli = struct
       & opt dir "./"
       & info ["d"; "working-dir"] ~doc)
 
-  let build = Term.(const build $ working_dir)
+  let build = Term.(const Build.build $ working_dir)
 
   let info =
     let doc = "Simple static blog generator written in OCaml." in
