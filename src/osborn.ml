d487b68e43b874f4888de53e48f62722e3068844
Antoine Luciani <antoine1fr@gmail.com>
2016-07-16 12:33:01 +0200

    Write conversion function for Mustache's JSON type
    Related to issue gh-1.

--- src/osborn.ml
+++ src/osborn.ml
@@ -1,5 +1,25 @@
 open Std
 
+let rec ezjsonm_value_of_yojson : Yojson.Safe.json -> Mustache.Json.value = function
+  | `Assoc x ->
+    let x' = List.map (fun (key, value) ->
+      (key, ezjsonm_value_of_yojson value)) x in
+    `O x'
+  | `Bool x -> `Bool x
+  | `Float x -> `Float x
+  | `Int x -> `Float (float_of_int x)
+  | `Intlit x -> `String x
+  | `Null -> `Null
+  | `String x -> `String x
+  | `Tuple x | `List x -> `A (List.map ezjsonm_value_of_yojson x)
+  | `Variant _ -> `Null
+
+let ezjsonm_of_yojson yojson : Mustache.Json.t =
+  match ezjsonm_value_of_yojson yojson with
+  | `A x -> `A x
+  | `O x -> `O x
+  | x -> `A [x]
+
 let build working_dir =
   let config = working_dir ^ "/config.sexp"
     |> Conf.from_file
